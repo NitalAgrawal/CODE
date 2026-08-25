252ae163baa4dd1e0cc5bbf72a6bbc6fd8bae5ce
Nital Agrawal <nitalagrawal5@gmail.com>
2026-08-25 16:46:58 +0530

    Time: 0 ms (100%), Space: 9 MB (40.45%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count=0;
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            
+            if(s[i]==' '){
+                count++;
+            }
+            if(count<k){
+                ans+=s[i];
+            }
+            else{
+                break;
+            }
+            
+        }
+        return ans;
+    }
+};
