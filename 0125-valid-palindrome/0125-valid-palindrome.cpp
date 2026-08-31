47b255c7b5477f93c6678ac99063b5e7d6e57f77
Nital Agrawal <nitalagrawal5@gmail.com>
2026-08-31 17:44:14 +0530

    Time: 3 ms (28.16%), Space: 10.3 MB (22.8%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(isalnum(s[i])) {
+              ans += tolower(s[i]);
+            }
+    }
+        
+
+        int l=0;
+        int r=ans.length()-1;
+        while(l<=r){
+            if(ans[l]== ans[r]){
+                l++;
+                r--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
