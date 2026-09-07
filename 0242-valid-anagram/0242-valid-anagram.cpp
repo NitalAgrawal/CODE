5cf8d521703f9535eda9672e5a84f72642418e5d
Nital Agrawal <nitalagrawal5@gmail.com>
2026-09-07 17:57:46 +0530

    Time: 2 ms (49.74%), Space: 9.5 MB (92.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int i=0;
+        while(i<s.length() ){
+            if(s[i] == t[i]){
+                i++;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
