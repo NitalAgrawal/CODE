08431bbb6473d21f13ac1306f8f7c47929715fe3
Nital Agrawal <nitalagrawal5@gmail.com>
2026-09-16 22:47:21 +0530

    Time: 0 ms (100%), Space: 8.4 MB (62.66%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int i=0;
+        int j=0;
+        while(i<word1.length() && j<word2.length()){
+           ans+=word1[i];
+           i++;
+           ans+=word2[j];
+           j++;
+        }
+
+        while(j<word2.length()){
+               ans+=word2[j];
+           j++;
+        }
+         while(i<word1.length()){
+             ans+=word1[i];
+           i++;
+        }
+        return ans;
+    }
+};
