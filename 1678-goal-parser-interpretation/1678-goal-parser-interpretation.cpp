e8933c2bcf80b401f6235b2f762772262ce3b4c6
Nital Agrawal <nitalagrawal5@gmail.com>
2026-09-17 23:22:45 +0530

    Time: 3 ms (42.56%), Space: 8.1 MB (77.3%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans ="";
+        for(int i=0;i<command.length();i++){
+            if(command[i]== 'G' ||command[i]== 'a' ||command[i]== 'l'){
+                ans+=command[i];
+            }
+            else if(command[i]== '(' && command[i+1]== ')'){
+                ans+='o';
+            }
+            else{
+                continue;
+            }
+        }
+        return ans ;
+    }
+};
